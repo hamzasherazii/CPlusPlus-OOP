15f375d9abbd4387cff9aa9255c4233b9aa3abf9
Hamza Sherazi <98487157+hamzasherazii@users.noreply.github.com>
2023-03-16 00:05:59 +0500

    completed first question

--- Assignment 1 - March 16 2023/q1.cpp
+++ Assignment 1 - March 16 2023/q1.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
 using namespace std;
 
+class Rectangle{
+    float height;
+    float width;
+
+    public:
+    void inputDetail(){
+        cout << "Enter height: ";
+        cin >> height;
+        cout << "Enter width: ";
+        cin >> width;
+    }
+    void findArea(){
+        float area = height * width;
+        cout << "Area: " << area;
+    }
+};
+
+
 int main()
 {
-    cout << "Testing compiler";
+    // cout << "Testing compiler";
+
+    Rectangle area;
+    area.inputDetail();
+    area.findArea();
 
     return 0;
 }
