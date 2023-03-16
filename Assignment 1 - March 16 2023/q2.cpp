90cae9398559fe6ce1eacf053adfae0a84990fa5
Hamza Sherazi <98487157+hamzasherazii@users.noreply.github.com>
2023-03-16 06:40:39 +0500

    question 2 completed

--- Assignment 1 - March 16 2023/q2.cpp
+++ Assignment 1 - March 16 2023/q2.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
 using namespace std;
 
-// 
+class Student
+{
+  int rollNumber;
+  long phoneNumber;
+  string address;
+
+public:
+  void condition(int roll, long phone, string addrs)
+  {
+    rollNumber = roll;
+    phoneNumber = phone;
+    address = addrs;
+  }
+
+  void displayData()
+  {
+    cout << "The roll no. of the student is " << rollNumber << endl;
+    cout << "The phone no of the student is " << phoneNumber << endl;
+    cout << "The address of the student is " << address << endl;
+  }
+};
 
 int main()
 {
-
+  Student sami, john;
+  cout << "STUDENT NAME: SAMI" << endl;
+  sami.condition(127, 43171046156023, "House # 1122, street # 12.");
+  sami.displayData();
+  cout << "------------------------------" << endl;
+  
+  cout << "STUDENT NAME: JOHN" << endl;
+  john.condition(126, 1285613445037, "House # 2211, street 21, phase 1, Etihad Town, Lahore.");
+  john.displayData();
+  return 0;
 }
---
+++ Assignment 1 - March 16 2023/q3.cpp
@@ -0,0 +1,1 @@
+// Assign and print the roll number, phone number and address of two students having names “Sami” and “John” respectively by creating two objects of the class "Student‟ 
